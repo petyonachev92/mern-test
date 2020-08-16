59429f661076dc5fe079e0942d92c88a01777dd4
Petyo Nachev <pyo@abv.bg>
2020-08-16 09:00:08 +0300

    Registration update

--- mern-app-test/backend/server.js
+++ mern-app-test/backend/server.js
@@ -1,10 +1,41 @@
-// const { request } = require('express')
-
 const express = require('express')
+const mongoose = require('mongoose')
+const User = require('./db_models/user')
+require('dotenv').config()
+const cors = require('cors')
+
+mongoose.connect(process.env.MONGO_URL, { useUnifiedTopology: true, useNewUrlParser: true })
+  .catch(err => console.log(err))
+
+const connection = mongoose.connection
+connection.once('open', () => {console.log('Connnection succesful!')})
+
 const app = express()
 const port = 5000
 
-app.get('/', (req, res) => res.send('paca'))
-app.get('/hui', (req, res) => res.send('send nudes'))
+const jsonParser = express.json()
 
+const corsConfig = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200
+}
+app.use(cors(corsConfig))
+
+app.get('/', (req, res) => res.send('home'))
 app.listen(port, () => console.log('Listening'))
+
+app.post('/registration', jsonParser, (req, res) => {
+  console.log(req.body)
+  const user = new User({
+    email: req.body.email,
+    password: req.body.password
+  })
+
+  if (User.findOne({ email: req.body.email }).exec()) {
+    res.send('There\'s a user already registered with this email!')
+  } else {
+    user.save()
+      .then(user => res.send('User is registered succesfully!'))
+      .catch(err => res.status(400).send('Unable to create user!'))
+  }
+})
--- mern-app-test/src/registration.js
+++ mern-app-test/src/registration.js
@@ -4,7 +4,22 @@ import { useForm } from 'react-hook-form'
 
 function Registration () {
   const { register, handleSubmit } = useForm()
-  const onSubmit = data => console.log(data)
+
+  const data = {
+    email: '',
+    password: ''
+  }
+
+  const onSubmit = (formData) => {
+    console.log(formData)
+    fetch('http://localhost:5000/registration', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData)
+    })
+      .then(resp => resp.json)
+      .then(console.log('User registered succesfully'))
+  }
 
   return (
     <div>
@@ -13,16 +28,24 @@ function Registration () {
           <label htmlFor="reg-email">
                         Email:
           </label>
-          <input id="reg-email"
+          <input name="email" id="reg-email"
             type="email"
             placeholder="email@email.com"
             ref={register({
               required: true,
+              unique: true,
               pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
             })}
           />
           <label htmlFor="reg-password">Password:</label>
-          <input id="reg-password" type="password" placeholder="Password"/>
+          <input name="password"
+            id="reg-password"
+            type="password"
+            placeholder="Password"
+            ref={register({
+              required: true
+            })}
+          />
           <label htmlFor="reg-rep-password">Repeat password:</label>
           <input id="reg-rep-password" type="password" placeholder="Repeat Password"/>
         </div>
